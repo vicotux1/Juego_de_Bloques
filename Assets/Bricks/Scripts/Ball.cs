dc0aa931d1d72cadec7d9fa0abc3f245518020cc
DarioMirandaYT <66534526+vicotux1@users.noreply.github.com>
2021-11-01 21:28:24 -0300

    Initial Comit
    Assets basicos.

--- Assets/Bricks/Scripts/Ball.cs
+++ Assets/Bricks/Scripts/Ball.cs
@@ -5,45 +5,31 @@ using UnityEngine;
 public class Ball : MonoBehaviour{
     Rigidbody rb2D;
     public float fuerzaDisparo;
-    bool IsGame;
+    public string ShootButton="Jump", tag_ground;
     public Vector3 Position_initial;
 
     void Start(){
         rb2D=GetComponent<Rigidbody>();
         transform.position=Position_initial;
-        IsGame=false;
         
     }
     void Update(){
             Shoot();
     }
     public void Shoot(){
-        if(IsGame=false && Input.GetButtonDown("Fire1")){
-       IsGame=true;
+        if(Input.GetButtonDown(ShootButton)){
        transform.SetParent(null);
        rb2D.isKinematic=false;
        Vector3 Force= new Vector3(0.8f,1.0f,0);
     rb2D.AddForce(Force*fuerzaDisparo);
-    }}
+        }
+    }
     void OnCollisionEnter(Collision other)
 	{
-		if (other.gameObject.CompareTag("Ground")) {
-        //StartCoroutine(Starting(0.5f)); 
+		if (other.gameObject.CompareTag(tag_ground)) {
         rb2D.velocity=Vector3.zero;
         transform.position=Position_initial;
-        //GameManager.gameManager.LifeCoint();
-        IsGame=false;
         Shoot();
 		}
-
-       /* IEnumerator Starting(float waitTime){
-            while (true){
-        rb2D.velocity=Vector3.zero;
-        transform.position=Position_initial;
-        GameManager.gameManager.LifeCoint();
-        IsGame=false;
-        Shoot();
-            }
-        }  */
     }
 }
--- Assets/Bricks/Scripts/Bricks.cs
+++ Assets/Bricks/Scripts/Bricks.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class Bricks : MonoBehaviour{
     public int BrickValue=10;
-    public string tag_Ball="ball";
-    private void Start() {
-    }
+    public string tag_ball="Ball";
 
-    void OnCollisionEnter(Collision other)
-	{
-		if (other.gameObject.CompareTag(tag_Ball)) 
-		{
+    void OnCollisionEnter(Collision other){
+		if (other.gameObject.CompareTag(tag_ball)) {
 		Destroy (this.gameObject);
-        //GameManager.gameManager.AddPoint(BrickValue);
-
-
-		}
+        GameManager.Points+=BrickValue;
+        }
     }
 }
